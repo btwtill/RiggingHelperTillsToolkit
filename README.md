027c6132ab7534c4c4f8982c332ba4afc1bea127
btwtill <Till.Pfarr@web.de>
2023-07-18 19:42:45 +0200

    IK FK Manual Switch support Working

--- README.md
+++ README.md
@@ -5,5 +5,17 @@
 
 TODO
 
-- IKFK Switch, enable usage within a parented Structure
+- automatically create IKFK Parent Joint with parent constraint
 - Add IKFK Attribute to swtich between IK and FK
+
+
+
+
+First shelf Install 
+
+import RiggingHelperTillsToolkit
+import importlib
+importlib.reload(RiggingHelperTillsToolkit)
+from RiggingHelperTillsToolkit import shelf_base
+importlib.reload(shelf_base)
+test = shelf_base.customShelf()
--- shelf_base.py
+++ shelf_base.py
@@ -66,7 +66,7 @@ class customShelf(_shelf):
         self.addButton(label="Test_Run", command=Run_Test)
         self.addButton(label="ShapeParent", command=ShapeParent)
         self.addButton(label="SamZero", command=insertNodeBefore)
-        self.addButton(label="IKFKSwitch", command=IKFKOpen)
+        self.addButton(label="IKFKSwitch", command=IKFKSwitch)
 
 
 
@@ -135,143 +135,146 @@ def insertNodeBefore(sfx = '_zro', alignToParent = False, loc = False, replace =
 
 
 ## IK FK Switch Functionallity
-name = mc.textField()
 def IKFKSwitch():
-    selectionList = mc.ls(selection=True)
+        selectionList = mc.ls(selection=True)
 
 
-    def duplicateSelection(selectionArray):
-        dupList = []
-        for i in selectionArray:
-            newJoint = mc.duplicate(i, parentOnly=True)
-            try:
-                mc.parent(newJoint, world=True)
-            except:
-                print("already world Parent")
-            dupList.append(newJoint)
-            
-        return dupList
-        
-    def renameList(targetList, name):
-        renamedList = []
-        for i in range(len(targetList)):
-            listElement = "".join(targetList[i])
-            newName = listElement.replace("1", "")
-            newName = name + newName
-            renamedList.append(newName)
-            mc.rename(targetList[i], newName)
-        return renamedList
-
-
-    def reparenting(targetArray):
-        for i in range(len(targetArray)):
-            if i != (len(targetArray) - 1):
-                mc.parent(targetArray[i + 1], targetArray[i])
+        def duplicateSelection(selectionArray):
+            dupList = []
+            for i in selectionArray:
+                newJoint = mc.duplicate(i, parentOnly=True)
+                try:
+                    mc.parent(newJoint, world=True)
+                except:
+                    print("already world Parent")
+                dupList.append(newJoint)
                 
-    def createIKChain(targetChain):
-        mc.select(targetChain[0], targetChain[len(targetChain) - 1])
-        IkHanldeName = "".join(targetChain[len(targetChain) - 1]) + "_IK_Handle"
-        mc.ikHandle(targetChain[0], targetChain[len(targetChain) - 1], name=IkHanldeName)
+            return dupList
+            
+        def renameList(targetList, name):
+            renamedList = []
+            for i in range(len(targetList)):
+                listElement = "".join(targetList[i])
+                newName = listElement.replace("1", "")
+                newName = name + newName
+                renamedList.append(newName)
+                mc.rename(targetList[i], newName)
+            return renamedList
 
-    def createIKFKSwitch(iKChain, fKChain, targetChain):
-        for i in range(len(targetChain)):
-            currentBlendNode = mc.pairBlend(node=targetChain[i], attribute=["tx", "ty", "tz", "rx", "ry", "rz"])
-            
-            ikNodeTranslationX = iKChain[i] + ".translateX"
-            ikNodeTranslationY = iKChain[i] + ".translateY"
-            ikNodeTranslationZ = iKChain[i] + ".translateZ"
-            
-            ikNodeRotationX = iKChain[i] + ".rotateX"
-            ikNodeRotationY = iKChain[i] + ".rotateY"
-            ikNodeRotationZ = iKChain[i] + ".rotateZ"
-            
-            fkNodeTranslationX = fKChain[i] + ".translateX"
-            fkNodeTranslationY = fKChain[i] + ".translateY"
-            fkNodeTranslationZ = fKChain[i] + ".translateZ"
-            
-            fkNodeRotationX = fKChain[i] + ".rotateX"
-            fkNodeRotationY = fKChain[i] + ".rotateY"
-            fkNodeRotationZ = fKChain[i] + ".rotateZ"
-            
-            currentBlendNodeTranslateX1 = currentBlendNode + ".inTranslateX1"
-            currentBlendNodeTranslateY1 = currentBlendNode + ".inTranslateY1"
-            currentBlendNodeTranslateZ1 = currentBlendNode + ".inTranslateZ1"
-            
-            currentBlendNodeRotationX1 = currentBlendNode + ".inRotateX1"
-            currentBlendNodeRotationY1 = currentBlendNode + ".inRotateY1"
-            currentBlendNodeRotationZ1 = currentBlendNode + ".inRotateZ1"
-            
-            
-            currentBlendNodeTranslateX2 = currentBlendNode + ".inTranslateX2"
-            currentBlendNodeTranslateY2 = currentBlendNode + ".inTranslateY2"
-            currentBlendNodeTranslateZ2 = currentBlendNode + ".inTranslateZ2"
-            
-            currentBlendNodeRotationX2 = currentBlendNode + ".inRotateX2"
-            currentBlendNodeRotationY2 = currentBlendNode + ".inRotateY2"
-            currentBlendNodeRotationZ2 = currentBlendNode + ".inRotateZ2"
-            
-            mc.connectAttr(ikNodeTranslationX, currentBlendNodeTranslateX1)
-            mc.connectAttr(ikNodeTranslationY, currentBlendNodeTranslateY1)
-            mc.connectAttr(ikNodeTranslationZ, currentBlendNodeTranslateZ1)
-            
-            mc.connectAttr(ikNodeRotationX, currentBlendNodeRotationX1)
-            mc.connectAttr(ikNodeRotationY, currentBlendNodeRotationY1)
-            mc.connectAttr(ikNodeRotationZ, currentBlendNodeRotationZ1)
-            
-            mc.connectAttr(fkNodeTranslationX, currentBlendNodeTranslateX2)
-            mc.connectAttr(fkNodeTranslationY, currentBlendNodeTranslateY2)
-            mc.connectAttr(fkNodeTranslationZ, currentBlendNodeTranslateZ2)
-            
-            mc.connectAttr(fkNodeRotationX, currentBlendNodeRotationX2)
-            mc.connectAttr(fkNodeRotationY, currentBlendNodeRotationY2)
-            mc.connectAttr(fkNodeRotationZ, currentBlendNodeRotationZ2)
+
+        def reparenting(targetArray):
+            for i in range(len(targetArray)):
+                if i != (len(targetArray) - 1):
+                    mc.parent(targetArray[i + 1], targetArray[i])
                     
-            
+        def createIKChain(targetChain):
+            mc.select(targetChain[0], targetChain[len(targetChain) - 1])
+            IkHanldeName = "".join(targetChain[len(targetChain) - 1]) + "_IK_Handle"
+            mc.ikHandle(targetChain[0], targetChain[len(targetChain) - 1], name=IkHanldeName)
+
+        def createIKFKSwitch(iKChain, fKChain, targetChain):
+            for i in range(len(targetChain)):
+                currentBlendNode = mc.pairBlend(node=targetChain[i], attribute=["tx", "ty", "tz", "rx", "ry", "rz"])
+                
+                ikNodeTranslationX = iKChain[i] + ".translateX"
+                ikNodeTranslationY = iKChain[i] + ".translateY"
+                ikNodeTranslationZ = iKChain[i] + ".translateZ"
+                
+                ikNodeRotationX = iKChain[i] + ".rotateX"
+                ikNodeRotationY = iKChain[i] + ".rotateY"
+                ikNodeRotationZ = iKChain[i] + ".rotateZ"
+                
+                fkNodeTranslationX = fKChain[i] + ".translateX"
+                fkNodeTranslationY = fKChain[i] + ".translateY"
+                fkNodeTranslationZ = fKChain[i] + ".translateZ"
+                
+                fkNodeRotationX = fKChain[i] + ".rotateX"
+                fkNodeRotationY = fKChain[i] + ".rotateY"
+                fkNodeRotationZ = fKChain[i] + ".rotateZ"
+                
+                currentBlendNodeTranslateX1 = currentBlendNode + ".inTranslateX1"
+                currentBlendNodeTranslateY1 = currentBlendNode + ".inTranslateY1"
+                currentBlendNodeTranslateZ1 = currentBlendNode + ".inTranslateZ1"
+                
+                currentBlendNodeRotationX1 = currentBlendNode + ".inRotateX1"
+                currentBlendNodeRotationY1 = currentBlendNode + ".inRotateY1"
+                currentBlendNodeRotationZ1 = currentBlendNode + ".inRotateZ1"
+                
+                
+                currentBlendNodeTranslateX2 = currentBlendNode + ".inTranslateX2"
+                currentBlendNodeTranslateY2 = currentBlendNode + ".inTranslateY2"
+                currentBlendNodeTranslateZ2 = currentBlendNode + ".inTranslateZ2"
+                
+                currentBlendNodeRotationX2 = currentBlendNode + ".inRotateX2"
+                currentBlendNodeRotationY2 = currentBlendNode + ".inRotateY2"
+                currentBlendNodeRotationZ2 = currentBlendNode + ".inRotateZ2"
+                
+                mc.connectAttr(ikNodeTranslationX, currentBlendNodeTranslateX1)
+                mc.connectAttr(ikNodeTranslationY, currentBlendNodeTranslateY1)
+                mc.connectAttr(ikNodeTranslationZ, currentBlendNodeTranslateZ1)
+                
+                mc.connectAttr(ikNodeRotationX, currentBlendNodeRotationX1)
+                mc.connectAttr(ikNodeRotationY, currentBlendNodeRotationY1)
+                mc.connectAttr(ikNodeRotationZ, currentBlendNodeRotationZ1)
+                
+                mc.connectAttr(fkNodeTranslationX, currentBlendNodeTranslateX2)
+                mc.connectAttr(fkNodeTranslationY, currentBlendNodeTranslateY2)
+                mc.connectAttr(fkNodeTranslationZ, currentBlendNodeTranslateZ2)
+                
+                mc.connectAttr(fkNodeRotationX, currentBlendNodeRotationX2)
+                mc.connectAttr(fkNodeRotationY, currentBlendNodeRotationY2)
+                mc.connectAttr(fkNodeRotationZ, currentBlendNodeRotationZ2)
+                        
+                
 
 
 
-    fkArray = duplicateSelection(selectionList)
+        fkArray = duplicateSelection(selectionList)
 
-    fkArray = renameList(fkArray, "FK_")
+        fkArray = renameList(fkArray, "FK_")
 
-    ikArray = duplicateSelection(selectionList)
+        ikArray = duplicateSelection(selectionList)
 
-    ikArray = renameList(ikArray, "IK_")
-
-
-    reparenting(fkArray)
-
-    reparenting(ikArray)
-
-    createIKChain(ikArray)
-
-    createIKFKSwitch(ikArray, fkArray, selectionList)
+        ikArray = renameList(ikArray, "IK_")
 
 
-def createIKFKAttribute(IKFKattributeName):
-        attribute = mc.spaceLocator()
-        mc.select(attribute)
-        attributeShape = "".join(mc.pickWalk(direction="down")) + ".L_Arm_iKfK"
-        mc.addAttr(longName=name, minValue=0, maxValue=1, defaultValue=0)
-        mc.setAttr(attributeShape, keyable=True)
+        reparenting(fkArray)
+
+        reparenting(ikArray)
+
+        createIKChain(ikArray)
+
+        createIKFKSwitch(ikArray, fkArray, selectionList)
 
 
 
-def BuildSwitch(*args):
+
+
+# def IKFKOpen():
+
+#     def BuildSwitch(*args):
+
+#         IKFKattributeName = mc.textField(name, query=True, text=True)
+
+#         createIKFKAttribute(IKFKattributeName)
+#         IKFKSwitch()
+
+#     window = mc.window( title="IKFKSwitch", iconName='IKFK', widthHeight=(200, 55), sizeable=True )
+#     mc.rowColumnLayout( numberOfColumns=2, columnAttach=(1, 'right', 0), columnWidth=[(1, 100), (2, 250)] )
+#     mc.text( label='Attribute_Name' )
+#     name = mc.textField()
+#     mc.textField( name, edit=True,)
+#     mc.button( label='Build Swtich', command=BuildSwitch )
+
+#     mc.showWindow(window)
+
+#     def createIKFKAttribute(IKFKattributeName):
+#         attribute = mc.spaceLocator()
+#         mc.select(attribute)
+#         attributeShape = "".join(mc.pickWalk(direction="down")) + ".L_Arm_iKfK"
+#         mc.addAttr(longName=name, minValue=0, maxValue=1, defaultValue=0)
+#         mc.setAttr(attributeShape, keyable=True)
+
     
-    IKFKattributeName = mc.textField(name, query=True, text=True)
-
-    createIKFKAttribute(IKFKattributeName)
-    IKFKSwitch()
-
-
-def IKFKOpen():
-    window = mc.window( title="IKFKSwitch", iconName='IKFK', widthHeight=(200, 55), sizeable=True )
-    mc.rowColumnLayout( numberOfColumns=2, columnAttach=(1, 'right', 0), columnWidth=[(1, 100), (2, 250)] )
-    mc.text( label='Attribute_Name' )
-    mc.textField( name, edit=True,)
-    mc.button( label='Build Swtich', command=BuildSwitch )
-
-    mc.showWindow(window)
 
 ############################
