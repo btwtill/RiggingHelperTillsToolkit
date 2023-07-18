7e8293d20e732acf89de49ebb7888b6e711b3ddf
btwtill <Till.Pfarr@web.de>
2023-07-18 16:17:19 +0200

    Added First Implementation of IK FK Switch

--- shelf_base.py
+++ shelf_base.py
@@ -1,9 +1,11 @@
 import maya.cmds as mc
 
+
+##DO NOTHING FUNCTION Base function used for passing a default action into a created button without command argument
 def _null(*args):
     pass
 
-
+##Base Shelf class with building block functions used for building your custom shelf
 class _shelf():
     '''A simple class to build shelves in maya. Since the build method is empty,
     it should be extended by the derived class to build the necessary shelf elements.
@@ -58,19 +60,20 @@ class _shelf():
 
 
 
-
+##Custom schelf buildl process add all the function you wannt into this function in order to build the shelf
 class customShelf(_shelf):
     def build(self):
         self.addButton(label="Test_Run", command=Run_Test)
         self.addButton(label="ShapeParent", command=ShapeParent)
         self.addButton(label="SamZero", command=insertNodeBefore)
+        self.addButton(label="IKFKSwitch", command=IKFKSwitch)
 
 
 
+##Functions to populate the shelf
 
 
-
-
+##ShapeParent Function 
 def ShapeParent(*args):
     selection = mc.ls(selection=True)
     PosOffset = True
@@ -78,12 +81,12 @@ def ShapeParent(*args):
     if(selection):
         mc.parent(selection[0], selection[1], shape=True, relative=PosOffset)
 
-
+##Test Function
 def Run_Test():
     print("Successful Installation")
 
 
-
+##Sam Zero Functionality 
 def insertNodeBefore(sfx = '_zro', alignToParent = False, loc = False, replace = '_ctl'):
     nodes = mc.ls(sl = 1)
 
@@ -127,3 +130,119 @@ def insertNodeBefore(sfx = '_zro', alignToParent = False, loc = False, replace =
                     mc.setAttr(node+attr, 0)
 
     return cnNodes
+
+
+
+
+## IK FK Switch Functionallity
+def IKFKSwitch():
+    selectionList = mc.ls(selection=True)
+
+
+    def duplicateSelection(selectionArray):
+        dupList = []
+        for i in selectionArray:
+            newJoint = mc.duplicate(i, parentOnly=True)
+            try:
+                mc.parent(newJoint, world=True)
+            except:
+                print("already world Parent")
+            dupList.append(newJoint)
+            
+        return dupList
+        
+    def renameList(targetList, name):
+        renamedList = []
+        for i in range(len(targetList)):
+            listElement = "".join(targetList[i])
+            newName = listElement.replace("1", "")
+            newName = name + newName
+            renamedList.append(newName)
+            mc.rename(targetList[i], newName)
+        return renamedList
+
+
+    def reparenting(targetArray):
+        for i in range(len(targetArray)):
+            if i != (len(targetArray) - 1):
+                mc.parent(targetArray[i + 1], targetArray[i])
+                
+    def createIKChain(targetChain):
+        mc.select(targetChain[0], targetChain[len(targetChain) - 1])
+        IkHanldeName = "".join(targetChain[len(targetChain) - 1]) + "_IK_Handle"
+        mc.ikHandle(targetChain[0], targetChain[len(targetChain) - 1], name=IkHanldeName)
+
+    def createIKFKSwitch(iKChain, fKChain, targetChain):
+        for i in range(len(targetChain)):
+            currentBlendNode = mc.pairBlend(node=targetChain[i], attribute=["tx", "ty", "tz", "rx", "ry", "rz"])
+            
+            ikNodeTranslationX = iKChain[i] + ".translateX"
+            ikNodeTranslationY = iKChain[i] + ".translateY"
+            ikNodeTranslationZ = iKChain[i] + ".translateZ"
+            
+            ikNodeRotationX = iKChain[i] + ".rotateX"
+            ikNodeRotationY = iKChain[i] + ".rotateY"
+            ikNodeRotationZ = iKChain[i] + ".rotateZ"
+            
+            fkNodeTranslationX = fKChain[i] + ".translateX"
+            fkNodeTranslationY = fKChain[i] + ".translateY"
+            fkNodeTranslationZ = fKChain[i] + ".translateZ"
+            
+            fkNodeRotationX = fKChain[i] + ".rotateX"
+            fkNodeRotationY = fKChain[i] + ".rotateY"
+            fkNodeRotationZ = fKChain[i] + ".rotateZ"
+            
+            currentBlendNodeTranslateX1 = currentBlendNode + ".inTranslateX1"
+            currentBlendNodeTranslateY1 = currentBlendNode + ".inTranslateY1"
+            currentBlendNodeTranslateZ1 = currentBlendNode + ".inTranslateZ1"
+            
+            currentBlendNodeRotationX1 = currentBlendNode + ".inRotateX1"
+            currentBlendNodeRotationY1 = currentBlendNode + ".inRotateY1"
+            currentBlendNodeRotationZ1 = currentBlendNode + ".inRotateZ1"
+            
+            
+            currentBlendNodeTranslateX2 = currentBlendNode + ".inTranslateX2"
+            currentBlendNodeTranslateY2 = currentBlendNode + ".inTranslateY2"
+            currentBlendNodeTranslateZ2 = currentBlendNode + ".inTranslateZ2"
+            
+            currentBlendNodeRotationX2 = currentBlendNode + ".inRotateX2"
+            currentBlendNodeRotationY2 = currentBlendNode + ".inRotateY2"
+            currentBlendNodeRotationZ2 = currentBlendNode + ".inRotateZ2"
+            
+            mc.connectAttr(ikNodeTranslationX, currentBlendNodeTranslateX1)
+            mc.connectAttr(ikNodeTranslationY, currentBlendNodeTranslateY1)
+            mc.connectAttr(ikNodeTranslationZ, currentBlendNodeTranslateZ1)
+            
+            mc.connectAttr(ikNodeRotationX, currentBlendNodeRotationX1)
+            mc.connectAttr(ikNodeRotationY, currentBlendNodeRotationY1)
+            mc.connectAttr(ikNodeRotationZ, currentBlendNodeRotationZ1)
+            
+            mc.connectAttr(fkNodeTranslationX, currentBlendNodeTranslateX2)
+            mc.connectAttr(fkNodeTranslationY, currentBlendNodeTranslateY2)
+            mc.connectAttr(fkNodeTranslationZ, currentBlendNodeTranslateZ2)
+            
+            mc.connectAttr(fkNodeRotationX, currentBlendNodeRotationX2)
+            mc.connectAttr(fkNodeRotationY, currentBlendNodeRotationY2)
+            mc.connectAttr(fkNodeRotationZ, currentBlendNodeRotationZ2)
+                    
+            
+
+
+
+    fkArray = duplicateSelection(selectionList)
+
+    fkArray = renameList(fkArray, "FK_")
+
+    ikArray = duplicateSelection(selectionList)
+
+    ikArray = renameList(ikArray, "IK_")
+
+
+    reparenting(fkArray)
+
+    reparenting(ikArray)
+
+    createIKChain(ikArray)
+
+    createIKFKSwitch(ikArray, fkArray, selectionList)
+
